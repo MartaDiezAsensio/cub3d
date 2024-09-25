f94e29e818d7545812eb8b5faf803739a0e17552
Gabriel Reus <Gabriel.Reus@gmail.com>
2024-09-25 21:22:50 +0200

    pre-merge

--- project/src/map/map_validator.c
+++ project/src/map/map_validator.c
@@ -6,26 +6,28 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/24 20:43:22 by gabriel           #+#    #+#             */
-/*   Updated: 2024/08/24 21:29:58 by gabriel          ###   ########.fr       */
+/*   Updated: 2024/09/25 21:20:36 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "map.h"
 
-/* First we check north and south walls, then */
-static	bool	map_validator_check_closed(t_map *map)
-{
-	size_t	i;
-	size_t	j;
-
-	i = 0;
-	j = 0;
-	while (i <)
-}
-
-
 bool	map_validator(t_map *map)
 {
+	size_t	x;
+	size_t	y;
 
+	y = 0;
+	while (y < map->height)
+	{
+		x = 0;
+		while (x < map->width)
+		{
+			if (map->map[x][y] == MAP_TILE_VOID)
+				continue;
+			x++;
+		}
+		y++;
+	}
 	return (true);
 }
